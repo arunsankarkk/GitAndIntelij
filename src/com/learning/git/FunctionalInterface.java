8401fd41d09f3f1983da2dd43bcc029f5e46fe22
Arun <arunsankarkk@gmail.com>
2017-12-18 23:43:17 -0500

    Second commit

---
+++ src/com/learning/git/FunctionalInterface.java
@@ -0,0 +1,6 @@
+package com.learning.git;
+
+public interface FunctionalInterface {
+
+    public int perform(int value1, int value2) ;
+}
--- src/com/learning/git/MainApp.java
+++ src/com/learning/git/MainApp.java
@@ -3,6 +3,13 @@ package com.learning.git;
 public class MainApp {
 
     public static void main(String args[]) {
-        System.out.print("This is class for git training with Intellij");
+        System.out.println("This is class for git training with Intellij");
+
+        FunctionalInterface funcIntf = (value1,value2) -> {
+            return value1+value2;
+        };
+        System.out.print("The Sum of two number is " +funcIntf.perform(10,20));
     }
+
+
 }
